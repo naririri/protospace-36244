f90b776c90b4cb0a295ff9054d756290c4f42ef6
naririri <nanananaririri@gmail.com>
2021-07-22 17:55:10 +0900

    コメント修正（empty,commet_name)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!,  only: [:edit, :new, :destroy, :create]
+  before_action :authenticate_user!,  only: [:edit, :new, :destroy, :create, :show]
   before_action :set_prototype,       only: [:show, :edit, :update, :destroy]
   #before_action :set_prototype_local,only: [:update, :destroy]
   before_action :move_to_index,       only: [:edit, :update, :destroy]
@@ -23,6 +23,7 @@ class PrototypesController < ApplicationController
 
   def show
     #@prototype = Prototype.find(params[:id])
+    #@user = User.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
